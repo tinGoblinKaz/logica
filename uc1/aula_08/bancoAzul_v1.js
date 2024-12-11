2dad72492f4bdb833a152d86b1251333885a5e86
Lucas <kasalves333@gmail.com>
2024-12-11 20:43:22 -0300

    banco azul v1.1

--- uc1/aula_08/bancoAzul_v1.js
+++ uc1/aula_08/bancoAzul_v1.js
@@ -1,3 +1,6 @@
+
+// julio, joão gabriel cerqueira, alice e lucas
+
 const limiteSaques = 3;
 let saldo = 0;
 let extrato = '';
@@ -14,41 +17,58 @@ while(true){
                 [q] - Sair
             ========================
             `);
-    if(menu.toLowerCase=='d'){
+
+
+    if(menu.toLowerCase() =='d'){
         let valor = parseFloat(prompt('Informe o valor do depósito: R$'));
-        if (valor >= 0){
+        if (valor > 0){
             saldo += valor;
-            extrato += `Depósito de R$ ${valor}`;
+            extrato += `Depósito de R$ ${valor} \n`;
             alert(`Depósito de R$ ${valor} realizado com sucesso`);
         }else{
             alert('@@@ Operação falhou! O valor informado é inválido! @@@');
         };
-    }else if(menu.toLowerCase =='s'){
-        if(numeroSaques<=3){
-            if (limite <= 500){
-                let valor = parseFloat(prompt('Informe o valor do saque: R$'))
+
+
+    }else if(menu.toLowerCase() =='s'){
+        if(numeroSaques < limiteSaques){
+            let valor = parseFloat(prompt('Informe o valor do saque: R$'))
+            if (valor <= limite){
                 if(valor <= saldo){
                     saldo -= valor;
-                    extrato -= `Saque de R$ ${valor}`;
+                    extrato += `Saque de R$ ${valor} \n`;
                     alert(`Saque de R$ ${valor} realizado com sucesso`)
                 }else{
-                    alert('@@@ Operação falhou! O valor informado é maior que o saldo! @@@ ');
+                    alert('@@@ Operação falhou! O valor informado é maior que o saldo! @@@');
                 };
                 
             }else{
                 alert('@@@ Operação falhou! O valor informado é maior que o seu limite atual! @@@');
                 };
-        }   numeroSaques++;
+            numeroSaques++;
+        }else if(numeroSaques >= limiteSaques){
+             alert('@@@ Operação falhou! Limite de Saques excedido @@@@');
+        }else{
+            alert('@@@@ Operação falhou! Erro imprevisto @@@')
+        };    
 
-    }else if(menu.toLowerCase=='e'){
+
+
+    }else if(menu.toLowerCase() =='e'){
         if (extrato == ''){
             alert('Não foram realizadas movimentações.');
         }else{
             alert(extrato);
         };
-    }else if(menu.toLowerCase == 'q'){
+
+       
+    }else if(menu.toLowerCase() == 'q'){
         break;
+    
     }else{
         alert('@@@@ Erro, ação inválida @@@')
     };
 };
+
+
+
